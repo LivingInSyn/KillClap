d3fc145b877dc9600d5d04440c7b37f69985f955
Jeremy Mill <jeremymill@gmail.com>
2016-09-20 11:31:57 -0400

    removed uncessary file

--- background.js
+++
@@ -1,6 +0,0 @@
-//background.js
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.duplicate(tab.id, function(tab){
-    chrome.tabs.executeScript(null, {file: "killClap.js"});
-  });
-});
